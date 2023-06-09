5c8e938e8543077253e8743e70770c50f2e8adc4
FijnStofGroep <fijnstofmeter@osinit.nl>
2023-06-09 16:56:03 +0200

    First commit

---
+++ airrohr-firmware/platformio_script.py
@@ -0,0 +1,25 @@
+Import("env")
+import hashlib
+import os
+import shutil
+
+def _file_md5_hexdigest(fname):
+    return hashlib.md5(open(fname, 'rb').read()).hexdigest()
+
+#!after_build function()
+def after_build(source, target, env):
+    if not os.path.exists("builds"):
+        os.mkdir("builds")
+
+    lang = env.GetProjectOption('lang')
+    target_name = lang.lower()
+
+    print("\n**** After build \"airrohr-firmware => SEN5X sensor\" **** ")
+ 
+    with open(f"builds/latest_{target_name}.bin.md5", "w") as md5:
+        print(_file_md5_hexdigest(target[0].path), file = md5)
+    shutil.copy(target[0].path, f"builds/latest_{target_name}.bin")
+
+    print( f"Copy: [\"{target[0].path}\"] To " + f"[\"./builds/latest_{target_name}.bin\"]")
+     
+env.AddPostAction("$BUILD_DIR/firmware.bin", after_build)
